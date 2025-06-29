4432c512aa2c0d6cb397457f776c8bc32916d549
nikhilm25 <nikhilmaan25@gmail.com>
2025-06-29 16:07:23 +0530

    fixing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,53 +1,33 @@
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.BufferedInputStream;
 import java.nio.ByteBuffer;
 
-import java.io.FileInputStream;
-
-
-
-
 public class Main {
   public static void main(String[] args){
     System.err.println("Logs from your program will appear here!");
-    
-     
+
     ServerSocket serverSocket = null; 
     Socket clientSocket = null;
     int port = 9092;
     try {
       serverSocket = new ServerSocket(port);
-      // Since the tester restarts your program quite often, setting SO_REUSEADDR
-      // ensures that we don't run into 'Address already in use' errors
       serverSocket.setReuseAddress(true);
-      // Wait for connection from client.
       clientSocket = serverSocket.accept();
 
-
       BufferedInputStream input = new BufferedInputStream(clientSocket.getInputStream());
-      
+
       byte[] messageSizeBytes = input.readNbytes(4);
       byte[] requestApiKeyBytes = input.readNbytes(2);
       byte[] requestApiVersionBytes = input.readNbytes(2);
       byte[] correlationIdBytes = input.readNbytes(4);
 
-      int messageSize = ByteBuffer.wrap(messageSizeBytes).getInt();
-      //int requestApiKey = ByteBuffer.wrap(requestApiKeyBytes).getInt();
-      //int requestApiVersion= ByteBuffer.wrap(requestApiVersionBytes).getInt();
-      int correlationId= ByteBuffer.wrap(correlationIdBytes).getInt();
-
-
-
+      int correlationId = ByteBuffer.wrap(correlationIdBytes).getInt();
 
       clientSocket.getOutputStream().write(messageSizeBytes);
-      
-      clientSocket.getOutputStream().write(correlationIdBytes);
-      
-
-
-
+      clientSocket.getOutputStream().write(ByteBuffer.allocate(4).putInt(correlationId).array());
 
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
@@ -62,3 +42,4 @@ public class Main {
     }
   }
 }
+
